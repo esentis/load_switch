2999dcada9287ed4447678eab4a8186d2374aa69
esentis <52862600+esentis@users.noreply.github.com>
2023-04-21 04:00:49 +0300

    🤡 Updates example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,8 +16,11 @@ class _MyAppState extends State<MyApp> {
   bool value = false;
 
   Future<bool> _getFuture() async {
+    print('Calling futute...');
     await Future.delayed(const Duration(seconds: 2));
-    return !value;
+    print('Future returned.');
+    value = !value;
+    return value;
   }
 
   @override
@@ -33,6 +36,43 @@ class _MyAppState extends State<MyApp> {
           LoadSwitch(
             value: value,
             future: _getFuture,
+            curveIn: Curves.easeInBack,
+            curveOut: Curves.easeOutBack,
+            animationDuration: const Duration(milliseconds: 500),
+            switchDecoration: (value) => BoxDecoration(
+              color: value ? Colors.green[100] : Colors.red[100],
+              borderRadius: BorderRadius.circular(30),
+              shape: BoxShape.rectangle,
+              boxShadow: [
+                BoxShadow(
+                  color: value
+                      ? Colors.green.withOpacity(0.2)
+                      : Colors.red.withOpacity(0.2),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: const Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+            spinColor: (value) => value
+                ? const Color.fromARGB(255, 41, 232, 31)
+                : const Color.fromARGB(255, 255, 77, 77),
+            spinStrokeWidth: 3,
+            thumbDecoration: (value) => BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(30),
+              shape: BoxShape.rectangle,
+              boxShadow: [
+                BoxShadow(
+                  color: value
+                      ? Colors.green.withOpacity(0.2)
+                      : Colors.red.withOpacity(0.2),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: const Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
             onChange: (v) {
               value = v;
               print('Value changed to $v');
