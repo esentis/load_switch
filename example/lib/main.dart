0baa6db617840e5a97119f2028307c4ade302d41
esentis <52862600+esentis@users.noreply.github.com>
2025-04-11 00:19:22 +0300

    ✨ Update main.dart to use LoadSwitchControllerExample and correct import path

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:example/styles/default_example.dart';
+import 'package:example/styles/controller_example.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: DefaultStyleExample()));
+  runApp(const MaterialApp(home: LoadSwitchControllerExample()));
 }
